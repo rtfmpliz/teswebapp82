67f3ad44380af261efad06c350e63e5f8a28e6f6
rtfmpliz <rtfmpliz@gmail.com>
2016-07-13 12:14:30 +0700

    Penambahan Debug/Release DBConnection

---
+++ ISOWebApps/Models/Karyawan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISOWebApps.Models
+{
+    public class Karyawan
+    {
+        public string NIK { get; set; }
+        public string NamaKaryawan { get; set; }
+
+    }
+}
---
+++ ISOWebApps/Models/Section.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISOWebApps.Models
+{
+    public class Section
+    {
+        public int ID { get; set; }
+        public string SectionDesc { get; set; }
+    }
+}
